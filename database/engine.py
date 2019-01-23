0a9d6e815bd35258519811af2672dcf232b7baf7
bunseokbot <bunseokbot@gmail.com>
2019-01-24 00:15:17 +0900

    Error solved while checking database debugging mode

--- database/engine.py
+++ database/engine.py
@@ -12,6 +12,6 @@ class Engine:
         Log.d("Creating database engine...")
         engine = create_engine(
             ini.read('DATABASE', 'URL'),
-            echo=True if ini.read('DATABASE', 'DEBUG') else False)
+            echo=True if ini.read('DATABASE', 'DEBUG') is 'true' else False)
         Base.metadata.create_all(bind=engine)
         return engine
