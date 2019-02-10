f6a03579a16b39dd51d53d873bab5a3debd03578
bunseokbot <bunseokbot@gmail.com>
2019-02-10 23:18:55 +0900

    Indentation problem solved at engine code

--- database/engine.py
+++ database/engine.py
@@ -10,8 +10,7 @@ class Engine:
     @classmethod
     def create(cls, ini):
         Log.d("Creating database engine...")
-        engine = create_engine(
-            ini.read('DATABASE', 'URL'),
+        engine = create_engine(ini.read('DATABASE', 'URL'),
             echo=True if ini.read('DATABASE', 'DEBUG') is 'true' else False)
         Base.metadata.create_all(bind=engine)
         return engine
